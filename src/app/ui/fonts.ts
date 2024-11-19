fd85c54dddfd59396e1ba489c9449117fe53c6ac
Antoine Sidot <Antoine.Sidot@lausanne.ch>
2024-11-19 16:24:10 +0100

    Fix Gothic 16

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -2,6 +2,6 @@ import {DotGothic16, Inter, Manrope} from 'next/font/google';
 
 export const manrope = Manrope({ subsets: ['latin']});
 
-export const dotGothic = DotGothic16({weight: '400'})
+export const dotGothic = DotGothic16({weight: '400', preload: false})
 
 export const inter = Inter({ subsets: ['latin'] });
